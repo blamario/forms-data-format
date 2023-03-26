ca00679a61dc2479aae0b69653c7abde50665efd
Mario Blažević <blamario@protonmail.com>
2023-03-26 09:17:22 -0400

    Header is not valid UTF-8, switched to ByteString

--- src/Text/FDF.hs
+++ src/Text/FDF.hs
@@ -9,15 +9,19 @@ module Text.FDF (FDF (FDF, body), Field (Field, name, value, kids),
                  parse, serialize) where
 
 import Control.Applicative ((<*), (<*>), (<|>), many, optional)
+import Data.ByteString (ByteString)
 import Data.Char (isSpace)
+import Data.Monoid.Instances.ByteString.UTF8 (ByteStringUTF8 (ByteStringUTF8))
+import Data.Monoid.Textual (toText)
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Text.Encoding (encodeUtf8)
 import Text.ParserCombinators.Incremental.LeftBiasedLocal
 
 data FDF = FDF {
-  header :: Text,
+  header :: ByteString,
   body :: Field,
-  trailer :: Text}
+  trailer :: ByteString}
   deriving (Show)
 
 data Field = Field {
@@ -48,12 +52,12 @@ traverseFieldWithKey :: Applicative f => ([Text] -> Text -> f Text) -> Field -> 
 traverseFieldWithKey f x@Field{name, value, kids} =
   Field name <$> traverse (f [name]) value <*> traverse (traverseFieldWithKey $ f . (name:)) kids
 
-serialize :: FDF -> Text
+serialize :: FDF -> ByteString
 serialize FDF{header, body, trailer} =
   "%FDF-1.2\n"
   <> header
   <> "<<\n/FDF\n"
-  <> serializeField body <> "\n"
+  <> encodeUtf8 (serializeField body) <> "\n"
   <> ">>\n"
   <> trailer
   <> "%%EOF\n"
@@ -66,42 +70,48 @@ serializeField Field{name, value, kids} =
   <> if null kids then "" else "/Kids[\n" <> Text.intercalate "\n" (serializeField <$> kids) <> "]\n"
   <> ">>"
 
-parse :: Text -> Either String FDF
-parse = verify . inspect . feedEof . flip feed parser
+parse :: ByteString -> Either String FDF
+parse = verify . inspect . feedEof . flip feed parser . ByteStringUTF8
   where verify (Right ([(parsed, "")], Nothing)) = Right parsed
         verify (Right _) = Left "Internal parse error"
         verify (Left err) = Left err
 
-parser :: Parser Text FDF
+parser :: Parser ByteStringUTF8 FDF
 parser = FDF
   <$ string "%FDF-1.2" <* lineEnd
-  <*> (manyTill line begin)
+  <*> extract (manyTill line begin)
   <* begin
   <* string "/FDF" <* lineEnd
   <*> field
   <* end
-  <*> (string "endobj" <> lineEnd
-       <> takeCharsWhile isSpace
-       <> string "trailer" <> lineEnd
-       <> manyTill line (string "%%EOF"))
+  <*> extract (string "endobj" <> lineEnd
+               <> takeCharsWhile isSpace
+               <> string "trailer" <> lineEnd
+               <> manyTill line (string "%%EOF"))
   <* string "%%EOF"
   <* optional lineEnd
 
-field :: Parser Text Field
+field :: Parser ByteStringUTF8 Field
 field = Field <$ begin
-  <*> (string "/T (" *> takeCharsWhile (`notElem` [')', '\r', '\n']) <* string ")" <* lineEnd) -- name
-  <*> optional (string "/V (" *> takeCharsWhile (`notElem` [')', '\r', '\n']) <* string ")" <* lineEnd) -- value
+  <*> strictText (string "/T (" *> takeCharsWhile (`notElem` [')', '\r', '\n']) <* string ")" <* lineEnd)
+  <*> optional (strictText $ string "/V (" *> takeCharsWhile (`notElem` [')', '\r', '\n']) <* string ")" <* lineEnd)
   <*> moptional (string "/Kids [" *> lineEnd *> many field <* string "]" <* lineEnd) -- kids
   <* end
 
-begin :: Parser Text ()
+begin :: Parser ByteStringUTF8 ()
 begin = skip (string "<<" *> lineEnd)
 
-end :: Parser Text ()
+end :: Parser ByteStringUTF8 ()
 end = skip (string ">>" *> optional lineEnd)
 
-line :: Parser Text Text
+line :: Parser ByteStringUTF8 ByteStringUTF8
 line = takeCharsWhile (`notElem` ['\r', '\n']) <> lineEnd
 
-lineEnd :: Parser Text Text
+lineEnd :: Parser ByteStringUTF8 ByteStringUTF8
 lineEnd = string "\r\n" <|> string "\r" <|> string "\n"
+
+strictText :: Parser ByteStringUTF8 ByteStringUTF8 -> Parser ByteStringUTF8 Text
+strictText = fmap $ toText (error . ("Invalid UTF-8 sequence: " ++) . show)
+
+extract :: Parser ByteStringUTF8 ByteStringUTF8 -> Parser ByteStringUTF8 ByteString
+extract = fmap $ \(ByteStringUTF8 bs) -> bs
